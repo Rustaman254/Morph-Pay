afa08530ec1b9aaa5272a659472d8fa2c51d6f80
Rustaman254 <ofury47@gmail.com>
2025-09-21 02:03:43 +0300

    changing component padding

--- web-client/app/page.tsx
+++ web-client/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <DashboardOne>
       <div className="flex flex-col items-start space-y-1 mb-6">
-        <h1 className="text-3xl font-bold text-white">Welcome To Morph Pay</h1>
-        <p className="text-lg text-gray-300">Your gateway to seamless crypto transactions.</p>
+        {/* <h1 className="text-3xl font-bold text-white">Welcome To Morph Pay</h1>
+        <p className="text-lg text-gray-300">Your gateway to seamless crypto transactions.</p> */}
       </div>
       <BuySellSend mode="swap" />
     </DashboardOne>
